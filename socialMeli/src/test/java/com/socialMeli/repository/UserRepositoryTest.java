8863f47928b75f0b1c791c95e05f0ad14b576fe9
fabsgl <fabian.gomezlaguna@mercadolibre.com>
2024-02-28 15:12:39 -0300

    fabi_T0003y4: Refactoring ServiceTest with Params + UserRepoTest

--- socialMeli/src/test/java/com/socialMeli/repository/UserRepositoryTest.java
+++ socialMeli/src/test/java/com/socialMeli/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.socialMeli.repository;
 
 import com.socialMeli.entity.User;
 import com.socialMeli.entity.UserType;
+import com.socialMeli.exception.NotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,14 +12,24 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 class UserRepositoryTest {
 
     UserRepository userRepository;
 
+    User vendor;
+    User client1;
+    User client2;
+
     @BeforeEach
     void setUp() {
         userRepository = new UserRepository();
+        //vendor = new User(1,"luis",List.of(2,3),List.of(),UserType.VENDOR);
+        client1 = new User(1,"Luciano Gonzalez",List.of(),List.of(8,10),UserType.CLIENT);
+        client2 = new User(2,"Sofia Fernandez",List.of(),List.of(8),UserType.CLIENT);
     }
 
     @Test
@@ -34,5 +45,14 @@ class UserRepositoryTest {
 
 
     //T-0003 y T-0004 -> US-0003
-
+    @Test
+    void getAllFollowersFoundTest() {
+        List<User> expectedUsersList = List.of(client1, client2);
+        List<User> actualUsers = userRepository.getAllFollowers(List.of(1,2));
+        assertEquals(expectedUsersList, actualUsers);
+    }
+    @Test
+    void getAllFollowersNotFoundTest() {
+        assertThrows(NotFoundException.class, () -> userRepository.getAllFollowers(List.of(1,-2)));
+    }
 }
--- socialMeli/src/test/java/com/socialMeli/service/UserServiceTest.java
+++ socialMeli/src/test/java/com/socialMeli/service/UserServiceTest.java
@@ -12,6 +12,9 @@ import com.socialMeli.repository.IUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.socialMeli.entity.UserType.CLIENT;
 import static com.socialMeli.entity.UserType.VENDOR;
@@ -37,11 +41,34 @@ class UserServiceTest {
     UserService userService;
     User vendorUser;
     User clientUser;
+    List<User> usuariosArrange;
+    List<UserVendorDto> usuariosArrangeOrderAsc;
+    List<UserVendorDto> usuariosArrangeOrderDesc;
+    List<UserVendorDto> usuariosArrangeOrderNull;
+
+    static Stream<Arguments> validFollowersValues(){
+        return Stream.of(Arguments.of("name_asc"),Arguments.of("name_desc"),null);
+    }
 
     @BeforeEach
     void setUp() {
         vendorUser = new User(1, "agustin", List.of(2, 3), List.of(), VENDOR);
         clientUser = new User(2, "diego", List.of(), List.of(1, 3), CLIENT);
+        usuariosArrange = List.of(  clientUser,
+                                    new User(3, "facundo", List.of(), List.of(1), CLIENT),
+                                    new User(4, "fabian", List.of(), List.of(1), CLIENT));
+
+        usuariosArrangeOrderNull = new ArrayList<>(List.of( new UserVendorDto(2,"diego"),
+                                                            new UserVendorDto(3, "facundo"),
+                                                            new UserVendorDto(4,"fabian")));
+
+        usuariosArrangeOrderAsc = new ArrayList<>(List.of(  new UserVendorDto(2,"diego"),
+                                                            new UserVendorDto(4,"fabian"),
+                                                            new UserVendorDto(3,"facundo")));
+
+        usuariosArrangeOrderDesc = new ArrayList<>(List.of( new UserVendorDto(3,"facundo"),
+                                                            new UserVendorDto(4,"fabian"),
+                                                            new UserVendorDto(2,"diego")));
         when(userRepository.findUserByUserId(anyInt())).thenReturn(Optional.of(vendorUser));
     }
 
@@ -51,13 +78,11 @@ class UserServiceTest {
         VendorFollowCountDto vendorFollowCountDto = userService.getFollowerCount(1);
         assertEquals(expectedVendorFollowerAccount, vendorFollowCountDto);
     }
-
     @Test
     void getFollowerCountUserNotFoundTest() {
         when(userRepository.findUserByUserId(anyInt())).thenReturn(Optional.empty());
         assertThrows(NotFoundException.class, () -> userService.getFollowerCount(0));
     }
-
     @Test
     void getFollowerCountUserNotVendorTest() {
         when(userRepository.findUserByUserId(anyInt())).thenReturn(Optional.of(clientUser));
@@ -65,47 +90,21 @@ class UserServiceTest {
     }
 
     //T-0003 y T-0004 -> US-0003
-    @Test
-    void getVendorFollowersOrderNullOk() {
+    @ParameterizedTest
+    @MethodSource("validFollowersValues")
+    void getVendorFollowersOrderNullOk(String order) {
         //Arrange
-        List<User> usuariosArrange = List.of(clientUser, new User(3, "facundo", List.of(), List.of(1), CLIENT));
-        when(userRepository.getAllFollowers(any())).thenReturn(usuariosArrange); //any() reemplaza List.of(2, 3)
-        FollowedListDto expectedFollowedList =
-                new FollowedListDto(1, "agustin",
-                                usuariosArrange.stream()
-                                .map(UserVendorDto::new)
-                                .toList());
-
-        FollowedListDto actual = userService.getVendorFollowers(1, null);   //act
-        assertEquals(expectedFollowedList, actual);                                     //assertion
-    }
-    @Test
-    void getVendorFollowersOrderNameAscOK(){
-        List<User> usuariosArrange = List.of(clientUser, new User(3, "facundo", List.of(), List.of(1), CLIENT));
+        FollowedListDto expectedFollowedList;
+        if (order == null){
+            expectedFollowedList = new FollowedListDto(1,"agustin",usuariosArrangeOrderNull);
+        }else if(order.equalsIgnoreCase("name_asc")) {
+            expectedFollowedList = new FollowedListDto(1,"agustin",usuariosArrangeOrderAsc);
+        }else {
+            expectedFollowedList = new FollowedListDto(1,"agustin",usuariosArrangeOrderDesc);
+        }
         when(userRepository.getAllFollowers(any())).thenReturn(usuariosArrange);
-
-        FollowedListDto expectedFollowedList =
-                new FollowedListDto(1, "agustin",
-                        usuariosArrange.stream()
-                                .map(UserVendorDto::new)
-                                .sorted(Comparator.comparing(UserVendorDto::getUserName))
-                                .toList());
-        FollowedListDto actual = userService.getVendorFollowers(1, "name_asc");   //act
-        assertEquals(expectedFollowedList, actual);                                            //assertion
-    }
-    @Test
-    void getVendorFollowersOrderNameDescOK(){
-        List<User> usuariosArrange = List.of(clientUser, new User(3, "facundo", List.of(), List.of(1), CLIENT));
-        when(userRepository.getAllFollowers(any())).thenReturn(usuariosArrange);
-
-        FollowedListDto expectedFollowedList =
-                new FollowedListDto(1, "agustin",
-                        usuariosArrange.stream()
-                                .map(UserVendorDto::new)
-                                .sorted(Comparator.comparing(UserVendorDto::getUserName).reversed())
-                                .toList());
-        FollowedListDto actual = userService.getVendorFollowers(1, "name_desc");   //act
-        assertEquals(expectedFollowedList, actual);                                            //assertion
+        FollowedListDto actual = userService.getVendorFollowers(1, order);   //act
+        assertEquals(expectedFollowedList, actual);                                 //assertion
     }
     @Test
     void getVendorFollowersNotFoundTest() {
