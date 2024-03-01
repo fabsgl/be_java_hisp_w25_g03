23560f319da41a87163f92b8251b105c8f5b3661
facundo-salvia <facundo.salvia@mercadolibre.com>
2024-03-01 14:52:39 -0300

    Integration tests 3

--- socialMeli/src/test/java/com/socialMeli/controller/PostControllerTest.java
+++ socialMeli/src/test/java/com/socialMeli/controller/PostControllerTest.java
@@ -7,11 +7,12 @@ import com.socialMeli.repository.IProductRepository;
 import com.socialMeli.repository.IUserRepository;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 
 @SpringBootTest
@@ -39,13 +41,23 @@ public class PostControllerTest {
 
     @BeforeEach
     void setUp() {
+        Period oneWeeksPeriod = Period.ofWeeks(1);
         postRepository.add(
                 new Post(1, LocalDate.now(), 1, 1, 12000.0, 8)
+        );
+        postRepository.add(
+                new Post(2, LocalDate.now().minus(oneWeeksPeriod), 2, 1, 12000.0, 8)
         );
         productRepository.add(
                 new Product(
                         1, "Silla Gamer", "Gamer", "Racer",
                         "Negra", "Completa"
+                )
+        );
+        productRepository.add(
+                new Product(
+                        2, "Gabinete 3x", "Gamer", "Negro",
+                        "Bequiet", "Completa"
                 )
         );
 
@@ -60,6 +72,29 @@ public class PostControllerTest {
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Silla Gamer")));
     }
+
+    @Test
+    void obtainLastPublicationsByTheFollowedVendorsTest_ok_orderAsc() throws Exception {
+
+        MvcResult mvcResult = this.mockMvc.perform(
+                        get("/products/followed/1/list").param("order", "date_asc")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = mvcResult.getResponse().getContentAsString();
+
+        JsonPath jsonPath = JsonPath.from(responseBody);
+
+        List<LocalDate> postDates = jsonPath.getList("$.posts[*].date", LocalDate.class);
+
+        // Verificar el orden ascendente
+        for (int i = 1; i < postDates.size(); i++) {
+            assertTrue(postDates.get(i).isAfter(postDates.get(i - 1)));
+        }
+    }
+
 
     @Test
     void obtainLastPublicationsByTheFollowedVendorsTest_throwsNotFound() throws Exception {
@@ -87,7 +122,7 @@ public class PostControllerTest {
                         get("/products/followed/-1/list")
                                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print()).andExpect(status().is4xxClientError())
-                ;
+        ;
         ;
     }
 
@@ -116,5 +151,28 @@ public class PostControllerTest {
                 .andExpect(content().string(containsString("Post creado")));
     }
 
+    @Test
+    void addPost_throws() throws Exception {
+        String request = "{\n" +
+                "    \"user_id\": -1,\n" +
+                "    \"date\": \"14-02-2024\",\n" +
+                "    \"product\": {\n" +
+                "        \"product_id\": 10,\n" +
+                "        \"product_name\": \"Silla Gamer\",\n" +
+                "        \"type\": \"Gamer\",\n" +
+                "        \"brand\": \"Racer\",\n" +
+                "        \"color\": \"Black&White\",\n" +
+                "        \"notes\": \"Special Edition\"\n" +
+                "    },\n" +
+                "    \"category\": 100,\n" +
+                "    \"price\": 1500.50\n" +
+                "}";
 
+        this.mockMvc.perform(
+                        post("/products/post")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(request))
+                .andDo(print()).andExpect(status().isBadRequest());
+
+    }
 }
